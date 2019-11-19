96d0037f999ba1eb170228fe76ac9802ee1ce67d
Denis <capkovicdenis@gmail.com>
2019-11-19 22:59:24 +0100

    Add finding path to root folder func

---
+++ core/config_loader.py
@@ -0,0 +1,85 @@
+import subprocess
+import os
+
+from enum import Enum
+from typing import Optional, Generator
+
+
+class IMPORTANT_FOLDERS(Enum):
+    CONFIG = "config"
+    LOCAL_CONFIG = "localconfig"
+    REPORT = "report"
+
+
+class RootDirectoryNotFound(Exception): pass
+
+POSSIBLE_FOLDERS = set([import_dir.value for import_dir in IMPORTANT_FOLDERS])
+
+def get_root_directory() -> str:
+    root_directory = get_potential_root_directory()
+    os.environ["GLM_PATH"] = root_directory
+
+    return root_directory
+
+def get_potential_root_directory() -> str:
+    root_directory = get_enviroment_directory()
+    if root_directory:
+        return root_directory
+
+    current_path = get_terminal_output("pwd")
+    output = is_config_directory(current_path)
+    root_directory = None if output else current_path
+    if root_directory:
+        return current_path
+
+    root_directory = get_git_root_directory()
+    if root_directory:
+        return root_directory
+
+    while current_path != "":
+        if is_config_directory(current_path):
+            return current_path
+        current_path = current_path[:current_path.rfind("/")]
+
+    raise RootDirectoryNotFound()
+
+def get_enviroment_directory() -> Optional[str]:
+    output = os.environ.get('GLM_PATH', '')
+    return None if output == "" else output
+
+def get_terminal_output(command: str) -> Optional[str]:
+    try:
+        command_list = [command]
+        if " " in command:
+            command_list = command.split(" ")
+        output = subprocess.check_output(command_list).decode("utf-8")[:-1]
+        if output:
+            return output
+        if output != "":
+            print(f"Wrong formatted $GLM_PATH, current value = {output}")
+    except:
+        return None
+
+def get_git_root_directory() -> Optional[str]:
+    return get_terminal_output("git rev-parse --show-toplevel")
+
+def is_config_directory(path: str) -> bool:
+    output = False
+    for file_name in os.listdir(path):
+        if file_name == IMPORTANT_FOLDERS.CONFIG.value:
+            output = IMPORTANT_FOLDERS.CONFIG
+            break
+        if file_name == IMPORTANT_FOLDERS.LOCAL_CONFIG.value:
+            output = IMPORTANT_FOLDERS.LOCAL_CONFIG
+            break
+        if file_name == IMPORTANT_FOLDERS.REPORT.value:
+            output = IMPORTANT_FOLDERS.REPORT
+            break
+
+    #TODO maybe all of the options are not equiavalent, if config is not present but localconfig is maybe it can run but an exception will be throw
+
+    return output != False
+
+
+if __name__ == "__main__":
+    print(get_root_directory())
