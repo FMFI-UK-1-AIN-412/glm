856598bdf2a7f890c9361fa3b23b3196d7f00e37
Denis Capkovic <capkovicdenis@gmail.com>
2020-04-14 13:41:18 +0200

    Change pulls cli arguments

--- cli/pulls.py
+++ cli/pulls.py
@@ -7,50 +7,41 @@ from student.utils import get_students_from_university_logins
 
 def pulls_handler(args: List[str]):
     parser = ArgumentParser(prog="pulls", description="pull requests manipulation")
-    parser.add_argument("--students", "-s", action="store")
-    local_pulls_parser_group = parser.add_argument_group(
-        "local", "arguments for manipulation of local pull requests"
+    parser.add_argument(
+        "--students",
+        "-s",
+        nargs="+",
+        action="store",
+        metavar="students",
+        help="List of students",
     )
-    local_pulls_parser_group.add_argument("--local", "-l", action="store_true")
-    remote_pulls_parser_group = parser.add_argument_group(
-        "remote", "arguments for manipulation of remote pull requests"
-    )
-    remote_pulls_parser_group.add_argument("--remote", "-r", action="store_true")
-    remote_pulls_parser_group.add_argument(
-        "--combine", choices=["replace", "merge"], action="store"
+    parser.add_argument(
+        "--cached", "-c", help="Load PR from localconfig", action="store_true"
     )
 
     parsed_args = parser.parse_args(args)
 
-    if hasattr(parsed_args, "func"):
-        parsed_args.func(parsed_args)
+    context = Context()
+
+    students = None
+
+    if parsed_args.students:
+        students = get_students_from_university_logins(context, parsed_args.students)
+
+    pulls = []
+    if parsed_args.cached:
+        from remote.pull_request.utils import get_local_pull_requests
+
+        pulls.extend(get_local_pull_requests(context, students))
     else:
-        context = Context()
+        from remote.pull_request.utils import get_remote_pull_requests
 
-        students = None
+        pulls.extend(get_remote_pull_requests(context, students))
+        for pull in pulls:
+            pull.save()
 
-        if parsed_args.students:
-            students = get_students_from_university_logins(
-                context, parsed_args.students.split(",")
-            )
-
-        if parsed_args.local:
-            from remote.pull_request.utils import get_local_pull_requests
-
-            pull_requests = get_local_pull_requests(context, students)
-            if len(pull_requests) == 0:
-                print("No pull requests")
-            else:
-                for pull in pull_requests:
-                    print(pull)
-        if parsed_args.remote:
-            from remote.pull_request.utils import get_remote_pull_requests
-
-            # TODO: you need to remove all the previous ones, or at least mark them as closed
-            pull_requests = get_remote_pull_requests(context, students)
-            if len(pull_requests) == 0:
-                print("No pull requests")
-            else:
-                for pull in pull_requests:
-                    print(pull)
-                    pull.save()
+    if len(pulls) == 0:
+        print("No pull requests")
+    else:
+        for pull in pulls:
+            print(pull)
